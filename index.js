e6cdf2e4eaa8ba2d08516cbd29304cf8791a8de2
hayward2007 <hayward2007@naver.com>
2025-07-21 10:04:59 +0900

    feat: added auto calibration

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+import { autoCalibrate } from "./src/math";
+
 let granted = false;
 
 const acc = { x: 0, y: 0, z: 0 };
 const gyro = { x: 0, y: 0, z: 0 };
+const ori = { x: 0, y: 0, z: 0 };
 
 async function requestPermissions() {
   try {
-    if (
-      typeof DeviceMotionEvent.requestPermission === "function" &&
-      typeof DeviceOrientationEvent.requestPermission === "function"
-    ) {
+    if (typeof DeviceMotionEvent.requestPermission === "function") {
       await DeviceMotionEvent.requestPermission().then((motionPerm) => {
         if (motionPerm !== "granted") {
           alert("Motion permission was denied.");
@@ -16,15 +16,6 @@ async function requestPermissions() {
           return false;
         }
       });
-      await DeviceOrientationEvent.requestPermission().then(
-        (orientationPerm) => {
-          if (orientationPerm !== "granted") {
-            alert("Orientation permission was denied.");
-            granted = false;
-            return false;
-          }
-        }
-      );
     }
     granted = true;
     return true;
@@ -51,28 +42,27 @@ async function startTracking() {
   const stopButton = document.getElementById("stopButton");
   stopButton.style.display = "inline-block";
 
-  // window.addEventListener("deviceorientation", (event) => {
-  //   gyro.x = event.alpha || 0;
-  //   gyro.y = event.beta || 0;
-  //   gyro.z = event.gamma || 0;
-  // });
-
   window.addEventListener("devicemotion", (event) => {
     if (event.accelerationIncludingGravity) {
       acc.x = event.accelerationIncludingGravity.x / 9.8 || 0;
       acc.y = event.accelerationIncludingGravity.y / 9.8 || 0;
       acc.z = event.accelerationIncludingGravity.z / 9.8 || 0;
     }
-  });
-
-  let gyroscope = new Gyroscope({ frequency: 50 });
-  gyroscope.addEventListener("reading", () => {
-    gyro.x = gyroscope.x || 0;
-    gyro.y = gyroscope.y || 0;
-    gyro.z = gyroscope.z || 0;
+    if (event.rotationRate) {
+      gyro.x = event.rotationRate.alpha / Math.PI || 0;
+      gyro.y = event.rotationRate.beta / Math.PI || 0;
+      gyro.z = event.rotationRate.gamma / Math.PI || 0;
+    }
   });
 
   setInterval(() => {
+    ({ acc, ori } = autoCalibrate(
+      new Vector3D(acc.x, acc.y, acc.z),
+      new Vector3D(gyro.x, gyro.y, gyro.z),
+      defaultVector,
+      50
+    ));
+
     document.getElementById("accX").textContent = `x: ${acc.x.toFixed(2)}`;
     document.getElementById("accY").textContent = `y: ${acc.y.toFixed(2)}`;
     document.getElementById("accZ").textContent = `z: ${acc.z.toFixed(2)}`;
