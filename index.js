561552005d5162d1aabc527b17c9db8ae0eb8a7d
hayward2007 <hayward2007@naver.com>
2025-07-21 10:18:45 +0900

    feat: imported in one file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import { autoCalibrate } from "./src/math";
-
 let granted = false;
+let interval;
 
 const acc = { x: 0, y: 0, z: 0 };
 const gyro = { x: 0, y: 0, z: 0 };
@@ -55,7 +54,8 @@ async function startTracking() {
     }
   });
 
-  setInterval(() => {
+  interval = setInterval(() => {
+    console.log("acc:", acc, "gyro:", gyro);
     ({ acc, ori } = autoCalibrate(
       new Vector3D(acc.x, acc.y, acc.z),
       new Vector3D(gyro.x, gyro.y, gyro.z),
@@ -66,15 +66,15 @@ async function startTracking() {
     document.getElementById("accX").textContent = `x: ${acc.x.toFixed(2)}`;
     document.getElementById("accY").textContent = `y: ${acc.y.toFixed(2)}`;
     document.getElementById("accZ").textContent = `z: ${acc.z.toFixed(2)}`;
-    document.getElementById("gyroX").textContent = `x (alpha): ${gyro.x.toFixed(
-      2
-    )}`;
-    document.getElementById("gyroY").textContent = `y (beta): ${gyro.y.toFixed(
-      2
-    )}`;
-    document.getElementById("gyroZ").textContent = `z (gamma): ${gyro.z.toFixed(
-      2
-    )}`;
+    document.getElementById("gyroX").textContent = `x (alpha): ${Math.sin(
+      ori.x
+    ).toFixed(2)}`;
+    document.getElementById("gyroY").textContent = `y (beta): ${Math.sin(
+      ori.y
+    ).toFixed(2)}`;
+    document.getElementById("gyroZ").textContent = `z (gamma): ${Math.sin(
+      ori.z
+    ).toFixed(2)}`;
   }, 50);
 }
 
@@ -85,7 +85,7 @@ function stopTracking() {
   startButton.style.display = "inline-block";
 
   window.removeEventListener("devicemotion", () => {});
-  window.removeEventListener("deviceorientation", () => {});
+  clearInterval(interval);
 
   acc.x = acc.y = acc.z = 0;
   gyro.x = gyro.y = gyro.z = 0;
@@ -98,20 +98,154 @@ function stopTracking() {
   document.getElementById("gyroZ").textContent = "z (gamma): 0.00";
 }
 
-document.getElementById("startButton").addEventListener("click", async () => {
-  if (granted) {
-    startTracking();
-    document.getElementById("startButton").style.display = "none";
-  } else {
-    await requestPermissions();
-    if (granted) {
-      startTracking();
-      document.getElementById("startButton").style.display = "none";
-    }
-  }
-});
+// document.getElementById("startButton").addEventListener("click", async () => {
+//   console.log("Start button clicked");
+//   if (granted) {
+//     startTracking();
+//     document.getElementById("startButton").style.display = "none";
+//   } else {
+//     await requestPermissions();
+//     if (granted) {
+//       startTracking();
+//       document.getElementById("startButton").style.display = "none";
+//     }
+//   }
+// });
 
-document.getElementById("stopButton").addEventListener("click", () => {
-  stopTracking();
-  document.getElementById("stopButton").style.display = "none";
-});
+// document.getElementById("stopButton").addEventListener("click", () => {
+//   stopTracking();
+//   document.getElementById("stopButton").style.display = "none";
+// });
+
+/**
+ * @typedef {number} Number
+ */
+
+/**
+ * 3D Vector class
+ */
+class Vector3D {
+  /** @type {number} */
+  x;
+  /** @type {number} */
+  y;
+  /** @type {number} */
+  z;
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   */
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  /**
+   * @returns {number}
+   */
+  magnitude() {
+    return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2);
+  }
+
+  /**
+   * @returns {Vector3D}
+   */
+  normalize() {
+    const mag = this.magnitude();
+    return new Vector3D(this.x / mag, this.y / mag, this.z / mag);
+  }
+
+  /**
+   * @param {Vector3D} other
+   * @returns {Vector3D}
+   */
+  subtract(other) {
+    return new Vector3D(this.x - other.x, this.y - other.y, this.z - other.z);
+  }
+
+  /**
+   * @param {Vector3D} other
+   * @returns {number}
+   */
+  dot(other) {
+    return this.x * other.x + this.y * other.y + this.z * other.z;
+  }
+
+  /**
+   * @param {Vector3D} other
+   * @returns {number}
+   */
+  cosineSimilarity(other) {
+    const dotProduct = this.dot(other);
+    const magnitudeA = this.magnitude();
+    const magnitudeB = other.magnitude();
+    return dotProduct / (magnitudeA * magnitudeB);
+  }
+}
+
+/** @type {Vector3D} */
+const defaultVector = new Vector3D(0, -1, 0);
+
+/**
+ * @typedef {Object} CalibrationResult
+ * @property {Vector3D} acceleration
+ * @property {Vector3D} orientation
+ */
+
+const initializeOrientaion = (acceleration) => {
+  acceleration = acceleration.normalize();
+  return new Vector3D(
+    -Math.PI - Math.atan2(acceleration.z, acceleration.y),
+    0,
+    Math.asin(acceleration.x)
+  );
+};
+
+const calculateGravity = (orientaion) => {
+  return new Vector3D(
+    -Math.sin(orientaion.z),
+    -Math.cos(orientaion.x) * Math.cos(orientaion.z),
+    Math.sin(orientaion.x) * Math.cos(orientaion.z)
+  );
+};
+
+const integrateGyro = (orientation, gyro, dt) => {
+  return new Vector3D(
+    orientation.x + (gyro.x * dt) / 1000,
+    0,
+    orientation.z + (gyro.z * dt) / 1000
+  );
+};
+
+const autoCalibrate = (acceleration, gyro, orientation, dt) => {
+  const threshold = 1.1;
+  /**
+   * @param {Vector3D} acceleration
+   * @param {Vector3D} gyro
+   * @param {Vector3D} orientation
+   * @param {number} dt
+   * @returns {CalibrationResult}
+   */
+  // cosine similarity should be more than 0.5(which means 60 degrees) since the error value magnifies as the angle converges to 90 degrees(x axis)
+  if (
+    acceleration.magnitude() < threshold &&
+    acceleration.cosineSimilarity(defaultVector) > 0.5
+  ) {
+    let _acceleration = acceleration.normalize();
+    orientation = new Vector3D(
+      -Math.PI - Math.atan2(_acceleration.z, _acceleration.y),
+      0,
+      -Math.asin(_acceleration.x)
+    );
+  } else {
+    orientation = integrateGyro(orientation, gyro, dt);
+  }
+
+  return {
+    acceleration: acceleration.subtract(calculateGravity(orientation)),
+    orientation: orientation,
+  };
+};
